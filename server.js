5b908bc8cf9d02485f50bf2ac8ebdbf364e245df
Nicholas Brown <znicholasbrown@gmail.com>
2017-10-01 14:08:45 -0400

    Init

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
+(()=>{
 
-var express = require('express');
-var app = express();
+'use strict';
 
-app.get("/", function (req, res) {
+const express = require('express');
+const app = express();
+const fs = require('fs');
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
 
-  res.send("Site is under construction... 👷");
-});
 
+app.use(express.static('public'));
+
+
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+})
+
+app.post("/upload", upload.single('test'), (req, res) => {
+  let response = { size: req.file.size };
+  fs.unlink(req.file.path, ()=> {  }); //removes the file after the information has been caught
+
+  res.send(response);
+})
 
 var listener = app.listen(process.env.PORT);
+
+
+
+})();
